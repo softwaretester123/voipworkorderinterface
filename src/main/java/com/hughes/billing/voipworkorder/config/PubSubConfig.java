6679defafd9c5cf9381885e2e57ecd17533d530b
Ashwin <ashwinathappank@gmail.com>
2023-09-19 09:45:50 +0530

    pub sub changes

--- src/main/java/com/hughes/billing/voipworkorder/config/PubSubConfig.java
+++ src/main/java/com/hughes/billing/voipworkorder/config/PubSubConfig.java
@@ -1,4 +1,20 @@
 package com.hughes.billing.voipworkorder.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
 public class PubSubConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(PubSubConfig.class);
+
+    @Value("${pubsub.project.id}")
+    String projectId;
+
+    @Value("${pubsub.topic.id}")
+    String topicIds;
+
+    @Value("${pubsub.subscription.id}")
+    String subscriptionIds;
+
+
 }
--- src/main/java/com/hughes/billing/voipworkorder/voipworkorderinterface/VoipworkorderinterfaceApplication.java
+++ src/main/java/com/hughes/billing/voipworkorder/voipworkorderinterface/VoipworkorderinterfaceApplication.java
@@ -2,12 +2,19 @@ package com.hughes.billing.voipworkorder.voipworkorderinterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VoipworkorderinterfaceApplication {
+public class VoipworkorderinterfaceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoipworkorderinterfaceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VoipworkorderinterfaceApplication.class);
 	}
 
 }
