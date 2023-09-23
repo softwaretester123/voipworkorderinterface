c7af3f2856b2ca38753194e4dae40f3dba1347aa
Ashwin <ashwinathappank@gmail.com>
2023-09-23 15:36:28 +0530

    Refactored VoipWorkOrderConstants.java

--- src/main/java/com/hughes/billing/voipworkorder/utils/VoipWorkOrderConstants.java
+++ src/main/java/com/hughes/billing/voipworkorder/utils/VoipWorkOrderConstants.java
@@ -2,8 +2,6 @@ package com.hughes.billing.voipworkorder.utils;
 
 public class VoipWorkOrderConstants {
 
-    public static final String MANDATORY_REQUEST_PARAM_MISSING = "Mandatory request parameter missing : ";
-
     public static final String ADD_VOIP_CONSTANT = "A";
 
     public static final String CANCEL_VOIP_CONSTANT = "CAN";
